a51c48cbf63973dcea68e795a66fb35d590155d7
Andrés Ramos Palacios <andreschennai@gmail.com>
2024-09-24 19:15:44 +0000

    hh

--- src/components/DownloadDialog.js
+++ src/components/DownloadDialog.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { Modal, Card, DatePicker, Select, Button } from "antd";
+import { Modal, Card, DatePicker, Select, Button, Spin } from "antd";
 import { useHeatmapLayersState } from "../states/HeatmapLayersState";
 import { usePlayingState } from "../states/PlayingState";
 import { downloadHeatmap } from "../services/api/download.service";
+import { componentTheme } from "../themes/blueTheme";
+
+const { Spin: spinTheme } = componentTheme.components;
 
 const { RangePicker } = DatePicker;
 const { Option } = Select;
@@ -10,6 +13,7 @@ const { Option } = Select;
 const DownloadDialog = ({ visible, onCancel }) => {
   const [rangoFechas, setRangoFechas] = useState([]);
   const [itemsSeleccionados, setItemsSeleccionados] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const { domains } = useHeatmapLayersState();
   const { dateFrom, dateTo } = usePlayingState();
@@ -24,6 +28,33 @@ const DownloadDialog = ({ visible, onCancel }) => {
 
   const onChangeSelect = (valor) => {
     setItemsSeleccionados(valor);
+  };
+
+  const processZip = (zipData, contentDisposition) => {
+    // Create a Blob from the ZIP data
+    const blob = new Blob([zipData], { type: "application/zip" });
+
+    // Default filename
+    let filename = "download.zip";
+
+    // Extract filename from Content-Disposition header if available
+    if (contentDisposition && contentDisposition.includes("filename=")) {
+      const filenameMatch = contentDisposition.match(
+        /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/
+      );
+      if (filenameMatch != null && filenameMatch[1]) {
+        filename = filenameMatch[1].replace(/['"]/g, "");
+      }
+    }
+
+    // Create a link element and trigger the download
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(link.href);
   };
 
   const onDownload = async () => {
@@ -41,7 +72,19 @@ const DownloadDialog = ({ visible, onCancel }) => {
         variables: [],
       });
     }
-    let result = await downloadHeatmap(downloadItems);
+    try {
+      setLoading(true);
+      let result = await downloadHeatmap(downloadItems);
+
+      const zipData = result.data;
+      const contentDisposition = result.headers["content-disposition"];
+
+      processZip(zipData, contentDisposition);
+      setLoading(false);
+      onCancel();
+    } catch (error) {
+      console.error("Download failed.", error);
+    }
   };
 
   return (
@@ -64,6 +107,19 @@ const DownloadDialog = ({ visible, onCancel }) => {
           style={{ width: "100%" }}
           options={domainOptions}
         ></Select>
+
+        {loading && (
+          <Spin tip="Descargando ficheros..." size="large" spinning={true}>
+            <div
+              style={{
+                padding: 30,
+                background: "rgba(0, 0, 0, 0.05)",
+                borderRadius: 4,
+              }}
+            />
+            ;
+          </Spin>
+        )}
       </Modal>
     </>
   );
--- src/services/api/download.service.js
+++ src/services/api/download.service.js
@@ -1,13 +1,35 @@
 const API_BASE_URL = "http://localhost:8080";
 
-export const downloadHeatmap = async (items) => {
-  let response = await fetch(`${API_BASE_URL}/api/download/heatmap`, {
+// export const downloadHeatmap = async (items) => {
+//   let response = await fetch(`${API_BASE_URL}/api/download/heatmap`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(items),
+//   });
+//   response = await response.json();
+//   return response;
+// };
+
+export const downloadHeatmap = (downloadItems) => {
+  return fetch(`${API_BASE_URL}/api/download/heatmap`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      // Include other headers if necessary
     },
-    body: JSON.stringify(items),
+    body: JSON.stringify(downloadItems),
+  }).then((response) => {
+    if (response.ok) {
+      return response.blob().then((blob) => ({
+        data: blob,
+        headers: {
+          "content-disposition": response.headers.get("Content-Disposition"),
+        },
+      }));
+    } else {
+      throw new Error("Network response was not ok");
+    }
   });
-  response = await response.json();
-  return response;
 };
